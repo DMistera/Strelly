4797e10148ac612940223bf7a9eef1a3d0b1f4be
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2022-01-24 00:41:08 +0100

    update columns

--- strelly-client/src/app/components/column/column.component.ts
+++ strelly-client/src/app/components/column/column.component.ts
@@ -1,11 +1,15 @@
 import { Column, Task} from '@app/models';
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { ColumnsService } from '@app/services/columns.service';
 import { MatDialog } from '@angular/material/dialog';
 import { NewColumnDialogComponent } from '../new-column-dialog/new-column-dialog.component';
 import { NewTaskDialogComponent } from '../new-task-dialog/new-task-dialog.component';
 import { TasksService } from '@app/services/tasks.service';
 import { Observable } from 'rxjs';
+
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { EditColumnDialogComponent } from '../edit-column-dialog/edit-column-dialog.component';
+import { DeleteColumnDialogComponent } from '../delete-column-dialog/delete-column-dialog.component';
 
 @Component({
   selector: 'app-column',
@@ -14,29 +18,65 @@ import { Observable } from 'rxjs';
 })
 export class ColumnComponent implements OnChanges {
   @Input() column: Column;
+  @Input() columns: Column[]|null;
   @Input() new = false;
+
+  @Output() public deleteColumnRequest: EventEmitter<any> = new EventEmitter<any>();
+
   tasks: Task[];
+  connectedTo = [] as any;
 
   constructor(private columnsService: ColumnsService, private tasksService: TasksService, public dialog: MatDialog){}
 
-  ngOnChanges(changes: SimpleChanges) {
-    console.log("changes"+JSON.stringify(changes));
+  ngOnChanges(changes: SimpleChanges){
     if(this.column){
-      console.log(JSON.stringify(this.column));
       this.tasksService.getTasks(this.column.id).subscribe(
         (data)=>{
           this.tasks = data[this.column.id];
+          this.column.tasks = this.tasks;
         }
       );
     }
+    this.getConnectedColumns();
+  }
+
+  getConnectedColumns(){
+    this.connectedTo = []
+    if(this.columns){
+      for(let col of this.columns){
+        this.connectedTo.push("column-"+col.id)
+      }
+    }
+    return this.connectedTo;
   }
 
   newColumnDialog() {
     const dialogRef = this.dialog.open(NewColumnDialogComponent);
     dialogRef.afterClosed().subscribe(result => {
-      console.log(typeof(result));
+      // console.log(typeof(result));
       if(result) {
-        this.columnsService.addColumns(result)
+        this.columnsService.addColumn(result)
+      }
+    });
+  }
+
+  editColumnDialog() {
+    const dialogRef = this.dialog.open(EditColumnDialogComponent,{data: this.column});
+    dialogRef.afterClosed().subscribe(result => {
+      // console.log(typeof(result));
+      if(result) {
+        this.columnsService.editColumn(result)
+      }
+    });
+  }
+
+  deleteColumnDialog() {
+    const dialogRef = this.dialog.open(DeleteColumnDialogComponent, {data: this.column});
+    dialogRef.afterClosed().subscribe(result => {
+      // console.log(typeof(result));
+      if(result) {
+        this.columnsService.deleteColumn(this.column.id);
+        this.deleteColumnRequest.emit();
       }
     });
   }
@@ -46,12 +86,32 @@ export class ColumnComponent implements OnChanges {
     dialogRef.afterClosed().subscribe(result => {
       // console.log(typeof(result));
       if(result) {
-        this.tasksService.addTasks(result.name, result.description, this.column.id);
+        this.tasksService.addTask(result.name, result.description, this.column.id);
       }
     });
   }
 
   removeTask(taskId: number){
     this.tasks = this.tasks.filter((t:Task)=> t.id != taskId)
+    this.column.tasks = this.tasks;
+  }
+
+  removeColumn(){
+    this.columnsService.deleteColumn(this.column.id);
+    this.deleteColumnRequest.emit();
+  }
+
+  drop(event: CdkDragDrop<any[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+    this.tasksService.editTask(event.container.data[event.currentIndex], Number(event.container.id.split("-")[1]), event.currentIndex)
   }
 }
