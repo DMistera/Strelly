635dc7ff61b0f8bed75d0a5ce703145883d0ebee
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2022-01-24 00:41:19 +0100

    update model

--- strelly-client/src/app/models/Column.ts
+++ strelly-client/src/app/models/Column.ts
@@ -1,9 +1,15 @@
+import { Task } from ".";
 export class Column {
   id: number;
   name: string;
+  order: number;
+  tasks: Task[];
 
   constructor(object: any) {
     this.id = object?.id || 0;
     this.name = object?.name || '';
+    this.order = object?.order || 0;
+    this.tasks = [];
+    object?.tasks?.forEach((obj:string) => { this.tasks.push(new Task(obj)); }) || [];
   }
 }
