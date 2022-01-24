4cd0c753e1b37633905178172bf8d9590a095160
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2022-01-24 19:14:02 +0100

    add assignUserToTaskDialog  and user icon tooltip

--- strelly-client/src/app/components/task/task.component.ts
+++ strelly-client/src/app/components/task/task.component.ts
@@ -4,6 +4,7 @@ import { Task } from '@app/models';
 import { MatDialog } from '@angular/material/dialog';
 import { EditTaskDialogComponent } from '../edit-task-dialog/edit-task-dialog.component';
 import { DeleteTaskDialogComponent } from '../delete-task-dialog/delete-task-dialog.component';
+import { AssignUserDialogComponent } from '../assign-user-dialog/assign-user-dialog.component';
 
 @Component({
   selector: 'app-task',
@@ -29,6 +30,16 @@ export class TaskComponent implements OnInit {
       console.log(typeof(result));
       if(result) {
         this.tasksService.editTask(result, this.task.column.id, this.task.order);
+      }
+    });
+  }
+
+  assignUserToTaskDialog(){
+    const dialogRef = this.dialog.open(AssignUserDialogComponent,{data: this.task});
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(typeof(result));
+      if(result) {
+        this.tasksService.assignUser(this.task, result);
       }
     });
   }
