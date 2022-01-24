a00cd7aa3fcce54875953cdd09d75ca513c64958
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2022-01-24 19:12:59 +0100

    update assign user dialog

--- strelly-client/src/app/components/assign-user-dialog/assign-user-dialog.component.ts
+++ strelly-client/src/app/components/assign-user-dialog/assign-user-dialog.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { User, Task } from '@app/models';
+import { UsersService } from '@app/services/users.service';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-assign-user-dialog',
@@ -6,10 +12,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assign-user-dialog.component.scss']
 })
 export class AssignUserDialogComponent implements OnInit {
+  users: User[];
+  
+  loadingData: boolean;
 
-  constructor() { }
+  myControl = new FormControl();
+  // users: User[] = [{name: 'Mary'}, {name: 'Shelley'}, {name: 'Igor'}];
+  filteredUsers: Observable<User[]>;
 
-  ngOnInit(): void {
+  constructor(@Inject(MAT_DIALOG_DATA) public task: Task, private usersService: UsersService, public dialogRef: MatDialogRef<AssignUserDialogComponent>) { 
+    this.loadingData = true;
+    this.usersService.getUsers().subscribe(
+      (data)=>{
+        console.log(data);
+        if(task.assignees.length>0){
+          for(let a of task.assignees){
+            data = data.filter((u:User)=>u.id != a.id);
+          }
+          this.users = data;
+        }
+        else{
+          this.users = data;
+        }
+        this.loadingData = false;
+      }
+    )
+  }
+  ngOnInit() {
+    this.filteredUsers = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => (typeof value === 'string' ? value : value.name)),
+      map(name => (name ? this._filter(name) : this.users.slice())),
+    );
   }
 
+  displayFn(user: User): string {
+    return user && user.userName ? user.userName : '';
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.users.filter(option => option.userName.toLowerCase().includes(filterValue));
+  }
+
+  closeDialog() {
+    if (this.myControl.valid) {
+      console.log(this.myControl);
+      this.dialogRef.close(this.myControl.value);
+    }
+  }
 }
