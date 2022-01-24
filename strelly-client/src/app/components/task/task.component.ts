43d8693406d3ad89edf2c0cdd1656aeaad0071a6
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2022-01-24 21:05:15 +0100

    update assigned users

--- strelly-client/src/app/components/task/task.component.ts
+++ strelly-client/src/app/components/task/task.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TasksService } from '@app/services/tasks.service';
-import { Task } from '@app/models';
+import { Task, User } from '@app/models';
 import { MatDialog } from '@angular/material/dialog';
 import { EditTaskDialogComponent } from '../edit-task-dialog/edit-task-dialog.component';
 import { DeleteTaskDialogComponent } from '../delete-task-dialog/delete-task-dialog.component';
@@ -40,8 +40,25 @@ export class TaskComponent implements OnInit {
       console.log(typeof(result));
       if(result) {
         this.tasksService.assignUser(this.task, result);
+        this.task.assignees.push(result);
       }
     });
+  }
+
+  // dismissUserDialog(){
+  //   const dialogRef = this.dialog.open(DismissUserDialogComponent,{data: this.task});
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     console.log(typeof(result));
+  //     if(result) {
+  //       this.tasksService.deassignUser(this.task, result);
+  //     }
+  //   });
+  // }
+
+
+  dismissUser(user: User){
+    this.tasksService.deassignUser(this.task, user);
+    this.task.assignees = this.task.assignees.filter((u: User)=>u.id!=user.id);
   }
 
   deleteTaskDialog(){
